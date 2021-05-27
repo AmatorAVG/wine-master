77c02b0eda3bcbeaa736d6d6209ce50cd1c33057
AmatorAVG <amatoravg@gmail.com>
2021-05-28 01:03:45 +0900

    After 2 review

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime, pandas
-import argparse
+import argparse, collections
 
 def main():
     env = Environment(
@@ -12,16 +12,14 @@ def main():
     template = env.get_template('template.html')
 
     parser = argparse.ArgumentParser(description='Программа наполнения сайта вин')
-    parser.add_argument('--path', help='Путь к таблице вин', default='~/devman/wine-master/wine_example.xlsx')
+    parser.add_argument('--path', help='Путь к таблице вин', default='wine_example.xlsx')
     args = parser.parse_args()
 
-    drinks_df = pandas.read_excel(args.path, keep_default_na=False)
-    raw_tree_drinks = drinks_df.to_dict(orient='records')
+    catalog = pandas.read_excel(args.path, keep_default_na=False, na_values=None).to_dict(orient='records')
+    drinks_by_categories = collections.defaultdict(list)
 
-    drinks_by_categories = drinks_df.groupby('Категория').groups
-
-    for cat, drinks in drinks_by_categories.items():
-        drinks_by_categories[cat] = [raw_tree_drinks[el] for el in drinks]
+    for product in catalog:
+        drinks_by_categories[product['Категория']].append(product)
 
     rendered_page = template.render(
         age=(datetime.datetime.now().year - 1920),
